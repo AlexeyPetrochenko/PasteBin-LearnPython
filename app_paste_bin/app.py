8c0cc972af38dee5bbb618314bfa97cd575cb4ff
AlexeyPetrochenko <Gyffi-99@yandex.ru>
2024-06-21 15:01:11 +0300

    Установил SQLAlchemy и Flask-SQLAlchemy, инициализировал БД-SQLlite и создал файл create_db для создания БД

--- app_paste_bin/app.py
+++ app_paste_bin/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
+from app_paste_bin.models import db
+
 
 def create_app(config_filename='config.py'):
     app = Flask(__name__)
     app.config.from_pyfile(config_filename)
+    db.init_app(app)
 
     @app.route('/')
     def index():
