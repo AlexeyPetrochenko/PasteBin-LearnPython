515eca26a51a6085eeedd1a711ae17a9b12bd4dd
AlexeyPetrochenko <Gyffi-99@yandex.ru>
2024-06-25 22:41:00 +0300

    Инициализированна БД PostgreSQL, установлен и инициализирован Flask-migrate, добавлена директория для моделей, изменен config.py

---
+++ app_paste_bin/db.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from app_paste_bin.config import SQLALCHEMY_DATABASE_URI
+
+
+engine = create_engine(url=SQLALCHEMY_DATABASE_URI)
+db_session = scoped_session(sessionmaker(bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+db = SQLAlchemy()
--- create_db.py
+++
@@ -1,6 +0,0 @@
-from app_paste_bin.models import db
-from app_paste_bin.app import create_app
-
-app = create_app()
-with app.app_context():
-    db.create_all()
